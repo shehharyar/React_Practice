6c4dd4e33afc97a474a8a7f176af567ea4e8ca76
shehharyar <sheharyarhassan1456@gmail.com>
2024-05-09 10:46:15 +0500

    changed modal's approach from imperative to declarative

--- Projects/PPlaces_app_(useEffect())/src/App.jsx
+++ Projects/PPlaces_app_(useEffect())/src/App.jsx
@@ -11,8 +11,8 @@ const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
 const storedPlaces= storedIds.map((id) => AVAILABLE_PLACES.find((place)=> place.id === id ));
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
@@ -29,12 +29,12 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+   setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+   setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -58,7 +58,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem(
@@ -69,7 +69,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
